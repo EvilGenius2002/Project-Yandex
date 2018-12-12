a3c1414f02123faaf2d98ff319f41bab7685b25b
MaximIvanoff <max.m.ivanoff@gmail.com>
2018-12-12 18:53:40 +0400

    Начало интерфейса

--- Yandex_Project.py
+++ Yandex_Project.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QLineEdit
+from PyQt5.QtGui import QFont
 
 
 class Example(QWidget):
@@ -11,6 +12,18 @@ class Example(QWidget):
         self.setGeometry(7, 31, 0, 0)
         self.setWindowTitle('Предсказатель наследования')
         self.setFixedSize(600, 500)
+        self.parent1 = QLineEdit(self)
+        self.parent1.setGeometry(165, 50, 110, 30)
+        self.parent1.setFont(QFont("Times", 18, QFont.Bold))
+        self.parent1.setText("AABBCC")
+        self.parent2 = QLineEdit(self)
+        self.parent2.setGeometry(325, 50, 110, 30)
+        self.parent2.setFont(QFont("Times", 18, QFont.Bold))
+        self.parent2.setText("AABBCC")
+        self.mainbutton = QPushButton(self)
+        self.mainbutton.setGeometry(260, 5, 80, 40)
+        self.mainbutton.setText("Прогноз")
+        self.mainbutton.setFont(QFont("Times", 13, QFont.Bold))
         self.show()
 
 
