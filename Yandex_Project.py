ff17f5b6a158ba77682bc086f654b4d59c186cf1
MaximIvanoff <max.m.ivanoff@gmail.com>
2018-12-12 18:37:52 +0400

    Basic_Window

--- Yandex_Project.py
+++ Yandex_Project.py
@@ -1,2 +1,20 @@
-#fgrwwrwgw
-print('hi')
+import sys
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(7, 31, 0, 0)
+        self.setWindowTitle('Предсказатель наследования')
+        self.setFixedSize(600, 500)
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
